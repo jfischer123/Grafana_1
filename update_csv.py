f1b54c7715a10ea9dc9fb176deecb97868eb58c9
jfischer123 <jan.fischer@automax-group.com>
2026-03-27 13:51:22 +0100

    Update update_csv.py

--- update_csv.py
+++ update_csv.py
@@ -13,7 +13,7 @@ def generuj_radky():
     praha = pytz.timezone("Europe/Prague")
     dnes = datetime.now(praha).strftime("%Y.%m.%d %H:%M")
     
-    hodnota = random.randint(10, 20)
+    hodnota = random.randrange(10, 201, 10)
     druh = random.choice(["Hotovost", "Platební karta", "Věrnostní karta"])
     
     return [{"Datum": dnes, "Hodnota": hodnota, "Druh platby": druh}]
